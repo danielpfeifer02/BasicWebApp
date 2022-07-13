80c8a1c6f146d649d88209aa5fe66ebd096f9705
ge32jel <pfeifer.daniel@gmx.at>
2022-07-13 11:11:50 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,7 +7,7 @@ public class QueryProcessor {
 
     public String process(String query) {
 		query = query.toLowerCase();
-        String[] queryList = query.split("%20");
+        String[] queryList = query.split(" ");
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
